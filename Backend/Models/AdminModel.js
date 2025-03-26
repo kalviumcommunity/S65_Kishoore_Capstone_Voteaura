7f6ff3ce01c21c85529f5e3abf25be578552f1d2
Kishoore <kishoore004@gmail.com>
2025-03-26 14:16:04 +0530

    Added mongoose model Schema

---
+++ Backend/Models/AdminModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const AdminSchema = new mongoose.Schema({
+    userid: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    district: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Admin', AdminSchema);
