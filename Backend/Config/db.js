b0e50af8f2dc49913626e2dd63e13eea83ab1964
Kishoore <kishoore004@gmail.com>
2025-04-01 15:12:33 +0530

    react set-up

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -4,9 +4,10 @@ const connectDB=async()=>{
     try{
         await mongoose.connect(process.env.MONGO_URI)
         console.log('MongoDB connected')
-    }catch(error){
-        console.log(error)
-    }
+    }catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1); 
+}
 }
 
-module.exports=connectDB;
+module.exports=connectDB
