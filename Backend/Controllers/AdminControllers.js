b354de16192d44bb0605115c1ae9ed8e7c26285a
Kishoore <kishoore004@gmail.com>
2025-04-04 14:22:15 +0530

    JWT

--- Backend/Controllers/AdminControllers.js
+++ Backend/Controllers/AdminControllers.js
@@ -1,16 +1,17 @@
+const jwt = require('jsonwebtoken')
 const Admin = require('../Models/AdminModel')
 
 const login = (req, res) => {
   try {
     const { userid, password, district } = req.body
-    console.log(`Received login request with userid: ${userid}, password: ${password}, district: ${district}`)
     if (userid === process.env.ADMIN_USER_ID && password === process.env.ADMIN_PASSWORD) {
-      return res.status(200).json({ message: 'Admin Logged In Successfully' })
+      const token = jwt.sign({ userid, district, role: 'admin' }, process.env.JWT_SECRET, { expiresIn: '1h' })
+      return res.status(200).json({ message: 'Admin Logged In Successfully', token })
     } else {
       return res.status(400).json({ message: 'Invalid Userid or Password' })
     }
   } catch (error) {
-    return res.status(400).json({ message: 'Invalid Userid or Password:', error: error.message })
+    return res.status(400).json({ message: 'Login failed', error: error.message })
   }
 }
 
